3d3c8e3229872502be0f31342fd73bcb9f2433a4
luisferfranco <luisferfranco@gmail.com>
2021-08-19 22:01:50 -0500

    Valores de acciones (final)

--- 10-stock/app.js
+++ 10-stock/app.js
@@ -1,10 +1,11 @@
 const graf = d3.select('#graf')
+const symbol = d3.select('#symbol')
 const anchoTotal = graf.style('width').slice(0, -2)
 const altoTotal = (anchoTotal * 9) / 16
 
-const tooltip = d3.select('#tooltip')
-const toolheader = d3.select('#tooltip-header')
-const toolbody = d3.select('#tooltip-body')
+const margin = { top: 20, right: 20, bottom: 70, left: 70 }
+const ancho = anchoTotal - margin.left - margin.right
+const alto = altoTotal - margin.top - margin.bottom
 
 const svg = graf
   .append('svg')
@@ -12,40 +13,54 @@ const svg = graf
   .attr('height', altoTotal)
   .attr('class', 'graf')
 
-const circle = svg
-  .append('circle')
-  .attr('cx', anchoTotal / 2)
-  .attr('cy', altoTotal / 2)
-  .attr('r', altoTotal / 4)
-  .attr('fill', '#800')
-  .on('mouseenter', (e) => showTooltip(e, 'Israel', 'IPC: 100,000'))
-  .on('mouseout', () => hideTooltip())
-  .on('mousemove', (e) => moveTooltip(e))
+const g = svg
+  .append('g')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
-function showTooltip(e, head, body) {
-  console.log('mouseenter')
-  console.log(d3.pointer(e))
+const x = d3.scaleTime().range([0, ancho])
+const y = d3.scaleLinear().range([alto, 0])
+const c = d3
+  .scaleOrdinal()
+  .domain(['amzn', 'nflx', 'tsla'])
+  .range(['#990000', '#009900', '#000099'])
+const parser = d3.timeParse('%Y-%m-%d')
 
-  toolheader.html(head)
-  toolbody.html(body)
+const xAxis = g.append('g').attr('transform', `translate(0, ${alto})`)
+const yAxis = g.append('g')
+const generadorLinea = d3
+  .line()
+  .x((d) => x(d.Date))
+  .y((d) => y(d.Close))
+const linea = g.append('path').attr('fill', 'none').attr('stroke-width', 2)
 
-  tooltip
-    .style('left', `${d3.pointer(e)[0] + 15}px`)
-    .style('top', `${d3.pointer(e)[1] + 15}px`)
-    .transition()
-    .duration(500)
-    .style('opacity', 1)
+function load(stock = 'amzn') {
+  d3.csv(`${stock}.csv`).then((data) => {
+    data.forEach((d) => {
+      d.Date = parser(d.Date)
+      d.Close = +d.Close
+    })
+
+    render(data, stock)
+  })
 }
 
-function hideTooltip() {
-  console.log('mouseout')
-  tooltip.transition().duration(500).style('opacity', 0)
+function render(data, stock = 'amzn') {
+  x.domain(d3.extent(data, (d) => d.Date))
+  y.domain([
+    d3.min(data, (d) => d.Close) * 0.95,
+    d3.max(data, (d) => d.Close) * 1.05,
+  ])
+
+  xAxis.call(d3.axisBottom().scale(x))
+  yAxis.call(d3.axisLeft().scale(y))
+
+  linea.attr('d', generadorLinea(data)).attr('stroke', c(stock))
 }
 
-function moveTooltip(e) {
-  console.log('mousemove')
-  console.log(d3.pointer(e))
-  tooltip
-    .style('left', `${d3.pointer(e)[0] + 15}px`)
-    .style('top', `${d3.pointer(e)[1] + 15}px`)
-}
+load()
+
+symbol.on('change', () => {
+  s = symbol.node().value
+  console.log(s)
+  load(s)
+})
